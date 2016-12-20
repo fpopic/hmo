f6cb4566f6d8fa8e4cbbd26591880c093c44edae
fpopic <filip.popic@gmail.com>
2016-12-20 14:16:46 +0100

    Initial commit (Google test added as lib only not separate project)

---
+++ lib/hmo/Node.h
@@ -0,0 +1,14 @@
+//
+// Created by fpopic on 20.12.16..
+//
+
+#ifndef HMO_NODE_H
+#define HMO_NODE_H
+
+
+class Node {
+
+};
+
+
+#endif //HMO_NODE_H
---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "hmo/Node.h"
+
+using namespace std;
+
+TEST(test, first) {
+    Node node;
+    EXPECT_EQ(1, 1);
+
+}
