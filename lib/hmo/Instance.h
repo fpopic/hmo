881473a4b8976504b2d8685029cdf2ea8e8e9bf1
fpopic <filip.popic@gmail.com>
2016-12-20 23:14:43 +0100

    Instance finished.

--- lib/hmo/Instance.h
+++ lib/hmo/Instance.h
@@ -5,11 +5,21 @@
 #ifndef HMO_PROJECT_INSTANCE_H
 #define HMO_PROJECT_INSTANCE_H
 
-// constants (for shorten public use)
+//region macro
+
+// broj posluzitelja
 #define NUM_SERVERS 28
+
+// broj komponenti virtualnih mreznih funkcija
 #define NUM_VMS 44
+
+// broj vrsta posluziteljskih resursa
 #define NUM_RES 2
+
+// broj cvorova
 #define NUM_NODES 8
+
+// broj usluznih lanaca
 #define NUM_SERVICE_CHAINS 62
 
 // two nodes: first <==> second
@@ -24,63 +34,50 @@
 // between two components
 #define BANDWITH 2
 
+//endregion
+
 struct Instance {
-
-    // broj posluzitelja
-    const static int numServers = 28;
-
-    // broj komponenti virtualnih mreznih funkcija
-    const static int numVms = 44;
-
-    // broj vrsta posluziteljskih resursa
-    const static int numRes = 2;
-
-    // broj cvorova
-    const static int numNodes = 8;
-
-    // broj usluznih lanaca
-    const static int numServiceChains = 62;
 
     // maksimalna potrosnja energije na posluoitelju
     // indeks oznacava broj posluzitelja
-    static const double P_max[numServers];
+    static const double P_max[NUM_SERVERS];
 
     // minimalna potrosnja energije na posluzitelju
     // ukoliko je utilizacija procesora 0%
-    static const double P_min[numServers];
+    static const double P_min[NUM_SERVERS];
 
     // dostupnost oba resursa na posluzitelju
     // index je oznaka posluzitelja
-    static const double req[numRes][numVms];
+    static const double req[NUM_RES][NUM_VMS];
 
     // dostupnost oba resursa na posluzitelju
     // index je oznaka posluzitelja
-    static const double av[numRes][numServers];
+    static const double av[NUM_RES][NUM_SERVERS];
 
     // lokacija posluzitelja na cvorovima
     // redak oznacava posluzitelj, a stupac cvor
-    static const bool al[numServers][numNodes];
+    static const bool al[NUM_SERVERS][NUM_NODES];
 
     // definicije usluznih lanaca
     // redak predstavlja lanac, a vrijednost 1 na i-tom mjestu u retku
     // oznacava ukljucenost komponente i u lanac
-    static bool sc[numServiceChains][numVms];
+    static bool sc[NUM_SERVICE_CHAINS][NUM_VMS];
 
     // potrosnja energije na cvorovima 1-8
-    static const int P[numNodes];
+    static const int P[NUM_NODES];
 
-    // definicija veza izmdju cvorova
+    // definicija veza izmedju cvorova
     // <prvi cvor, drugi cvor, kapacitet, potrosnja energije, kasnjenje>
     // <int, int, int, double, int>
     static const double Edges[][5];
 
-    // zahtijevana propusnost izmedju komponenti koje komuniciraju
+    // zahtijevana propusnost izmedju komponenti koje komuniciraju
     // <komponenta1, komponenta2, propusnost>
     static const int VmDemands[][3];
 
     // maksimalno dopusteno kasnjenje za svaki usluzni lanac
     // indeks je oznaka usluznog lanca
-    static const int lat[numServiceChains];
+    static const int lat[NUM_SERVICE_CHAINS];
 };
 
 #endif //HMO_PROJECT_INSTANCE_H
